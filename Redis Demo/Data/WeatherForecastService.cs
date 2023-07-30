8275b3d18069eb2c393e9ebb7fa67124da0f4d5a
S Hasanabadi <s.hasanabadi2022@gmail.com>
2023-07-30 11:56:02 +0430

    Add project files.

---
+++ Redis Demo/Data/WeatherForecastService.cs
@@ -0,0 +1,21 @@
+namespace Redis_Demo.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public async Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        {
+            await Task.Delay(1500);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
